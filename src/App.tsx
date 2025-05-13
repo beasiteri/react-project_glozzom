a9d89eb7ab90202a1b13226121655bef3b6276b7
beasiteri <siteribea@gmail.com>
2025-05-13 09:04:26 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
+import About from "./pages/about/About";
+import Services from "./pages/services/Services";
+import Blog from "./pages/blog/Blog";
+import Contact from "./pages/contact/Contact";
 
 function App() {
   return (
     <>
-      <div>App</div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
+      </Router>
     </>
   );
 }
